2fb9de85b25fc515985ba1a2d06f46ec9818c5f2
lucas2106 <80073835+lucas2106@users.noreply.github.com>
2025-02-24 10:16:26 -0300

    receitas entidade

--- src/entities/RecipeIngredient.ts
+++ src/entities/RecipeIngredient.ts
@@ -1,26 +1,18 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from "typeorm";
-import { Recipe } from "./Recipe";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-@Entity("recipes_ingredients") // Nome da tabela no banco
+@Entity("recipes_ingredients")
 export class RecipeIngredient {
-    
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column({ type: "varchar", length: 200, nullable: false })
-  name: string;
+    @Column({ type: "varchar", length: 100, nullable: false })
+    name: string;
 
-  @Column({type: 'int', nullable: false})
-  recipe_id: number
+    @CreateDateColumn({ type: "timestamp" })
+    created_at: Date;
 
-  @CreateDateColumn({ type: "timestamp" })
-  created_at: Date;
-
-  @UpdateDateColumn({ type: "timestamp" })
-  updated_at: Date;
-
-  @ManyToOne(() => Recipe, recipe => recipe.ingredients)
-  @JoinColumn({ name: "recipe_id" })
-  recipe: Recipe;
+    @UpdateDateColumn({ type: "timestamp" })
+    updated_at: Date;
+}
 
 }
